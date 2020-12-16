822d5f2ffa400083ff5fda2a4c4ad15f723d3b97
Jenny <69027218+jennn613@users.noreply.github.com>
2020-12-16 17:54:30 +0900

    scroll

--- browser/chatper2_coordinate/coordinate.js
+++ browser/chatper2_coordinate/coordinate.js
@@ -9,3 +9,19 @@ box.addEventListener('click', event=> {
     console.log(`page: ${event.pageX, event.pageY}`);
 
 });
+
+const buttonBox = document.querySelector('.buttonBox');
+const buttons = buttonBox.querySelectorAll('button');
+
+buttons[0].addEventListener('click', ()=> {
+    window.scrollBy({top: 100, left:0, behavior: 'smooth'});
+});
+
+buttons[1].addEventListener('click', ()=> {
+    // window.scrollTo(0, 100)
+    window.scrollTo({top: 100, left:0, behavior: 'smooth'});
+});
+
+buttons[2].addEventListener('click', ()=> {
+box.scrollIntoView();
+});
